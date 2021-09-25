b01f89968b14a2737c46f15580a3db5470cf2856
rafaelfontes777 <rafaelfontes777@hotmail.com>
2021-09-25 17:05:20 -0300

    system.properties file added

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,10 +1,16 @@
 package one.digitalinnovation.personapi.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("api/v1/people")
 public class PersonController {
+
+    @GetMapping
+    public String getBook() {
+        return "API test";
+    }
 }
