2a120915d148e056cd3ae4ab0fbec3ac5a753957
rafaelfontes777 <rafaelfontes777@hotmail.com>
2021-09-25 12:09:10 -0300

    Project start

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.personapi.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class PersonController {
+}
