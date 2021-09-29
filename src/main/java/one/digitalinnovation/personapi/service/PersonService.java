997339efac8db03899c6b1cbe7cbf1d03d642bd8
rafaelfontes777 <rafaelfontes777@hotmail.com>
2021-09-28 23:24:14 -0300

    Developed delete by Id operation

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -44,8 +44,19 @@ public class PersonService {
 
     public PersonDTO findById(long id) throws PersonNotFoundException {
         //Optional<Person> optionalPerson = personRepository.findById(id);
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExists(id);
         return personMapper.toDTO(person);
+    }
+
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+
+    private Person verifyIfExists(long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
     }
 }
